993d8a5b3fe2eab259a1a8d2856ecb5a62a599ef
ahmedmansour <a.a2051995@gmail.com>
2021-06-02 17:25:34 +0200

    adding the method to draw the graphs of titanic data set

--- dataAnalysis/src/main/java/textprocess/io/TittianicDOA.java
+++ dataAnalysis/src/main/java/textprocess/io/TittianicDOA.java
@@ -1,12 +1,30 @@
 package textprocess.io;
 
+import java.awt.Color;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.CategoryChartBuilder;
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.PieChartBuilder;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.style.Styler;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import tech.tablesaw.api.Table;
 import tech.tablesaw.io.csv.CsvReadOptions;
 
 public class TittianicDOA {
-public static void readcsv(String filename) {
+public  void readcsv(String filename) {
 //	DataFrame<Object> titaincs_row = null;
 //	try {
 //		titaincs_row = DataFrame.readCsv("G:\\exported data\\iti\\Java & UML Programming\\titanic-passengers.csv");
@@ -17,8 +35,8 @@ public static void readcsv(String filename) {
 	
 	CsvReadOptions.Builder builder = 
 			CsvReadOptions.builder(filename)
-				.separator(';')										// table is tab-delimited
-				.header(true).maxNumberOfColumns(12)											// no header
+				.separator(';')									// table is tab-delimited
+				.header(true).maxNumberOfColumns(Integer.valueOf(12))											// no header
 				;  				// the date format to use. 
 
 		CsvReadOptions options = builder.build();
@@ -33,5 +51,83 @@ public static void readcsv(String filename) {
 
 	System.out.println(t1.columns());
 	System.out.println(t1.summary());
+}
+public  List<TitanicPassenger> getPassengersFromJsonFile() {
+	List<TitanicPassenger> allPassengers = new Vector<TitanicPassenger>();
+    ObjectMapper objectMapper = new ObjectMapper ();
+    objectMapper.configure (DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    try (InputStream input = new FileInputStream ("src/main/resources/titanic_csv.json")) {
+        //Read JSON file
+        allPassengers = objectMapper.readValue (input, new TypeReference<List<TitanicPassenger>> () {
+        });
+    } catch (FileNotFoundException e) {
+        e.printStackTrace ();
+    } catch (IOException e) {
+        e.printStackTrace ();
+    }
+    return allPassengers;
+}
+public void graphPassengerAges(List<TitanicPassenger> passengerList) {
+	//filter to get an array of passenger ages
+	
+	}
+public void graphPassengerAgesDraw(List<TitanicPassenger> passengerList) {
+	List<Double> pAges = passengerList.stream ().map (TitanicPassenger::getAge).collect (Collectors.toList ());
+	List<String> pNames = passengerList.stream ().map (TitanicPassenger::getName).collect (Collectors.toList ());
+	//Using XCart to graph the Ages 1.Create Chart
+	CategoryChart chart = new CategoryChartBuilder ().width (1024).height (768).title ("Age Histogram").xAxisTitle ("Names").yAxisTitle
+	("Age").build ();
+	// 2.Customize Chart
+	chart.getStyler ().setLegendPosition(Styler.LegendPosition.InsideNW);
+	chart.getStyler ().setHasAnnotations (true);
+	chart.getStyler ().setStacked (true);
+	// 3.Series
+	chart.addSeries ("Passenger's Ages", pNames, pAges);
+	// 4.Show it
+	new SwingWrapper (chart).displayChart ();
+	}
+
+public  void graphPassengerClass(List<TitanicPassenger> passengerList) {
+	Map<String,Long> pclasses = passengerList.stream ().collect (Collectors.groupingBy(TitanicPassenger::getPclass,Collectors.counting()));
+
+// Create Chart
+PieChart chart = new PieChartBuilder ().width (800).height (600).title (getClass ().getSimpleName ()).build ();
+// Customize Chart
+Color[] sliceColors = new Color[]{new Color (180, 68, 50), new Color (130, 105, 120), new Color (80, 143, 160)};
+chart.getStyler ().setSeriesColors (sliceColors);
+// Series
+chart.addSeries ("First Class", pclasses.get("1"));
+chart.addSeries ("Second Class", pclasses.get ("2"));
+chart.addSeries ("Third Class", pclasses.get ("3"));
+// Show it
+new SwingWrapper (chart).displayChart ();
+}
+public  void graphPassengerSurvived(List<TitanicPassenger> passengerList) {
+	Map<String,Long> pclasses = passengerList.stream ().collect (Collectors.groupingBy(TitanicPassenger::isSurvived,Collectors.counting()));
+
+// Create Chart
+PieChart chart = new PieChartBuilder ().width (800).height (600).title (getClass ().getSimpleName ()).build ();
+// Customize Chart
+Color[] sliceColors = new Color[]{new Color (180, 68, 50), new Color (130, 105, 120), new Color (80, 143, 160)};
+chart.getStyler ().setSeriesColors (sliceColors);
+// Series
+chart.addSeries ("survived", pclasses.get("1"));
+chart.addSeries ("not survived", pclasses.get ("0"));
+// Show it
+new SwingWrapper (chart).displayChart ();
+}
+public  void graphPassengerSurvivedGender(List<TitanicPassenger> passengerList) {
+	Map<String,Long> pclasses = passengerList.stream ().filter(m->m.isSurvived().equals("1")).collect (Collectors.groupingBy(TitanicPassenger::getSex,Collectors.counting()));
+
+// Create Chart
+PieChart chart = new PieChartBuilder ().width (800).height (600).title (getClass ().getSimpleName ()).build ();
+// Customize Chart
+Color[] sliceColors = new Color[]{new Color (180, 68, 50), new Color (130, 105, 120), new Color (80, 143, 160)};
+chart.getStyler ().setSeriesColors (sliceColors);
+// Series
+chart.addSeries ("female survived", pclasses.get("female"));
+chart.addSeries ("male survived", pclasses.get ("male"));
+// Show it
+new SwingWrapper (chart).displayChart ();
 }
 }
